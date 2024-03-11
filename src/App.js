e162e9652b9e1ae4bed3bc0d6c170f40c2e8b3c4
권지현 <kjh2868@duse.co.kr>
2024-03-11 18:02:55 +0900

    react test dwc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+// import "./App.css";
+import "duse-wepcomponents";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <dwc-textfield label="durls" helperText="ddd"></dwc-textfield>
         <a
           className="App-link"
           href="https://reactjs.org"
